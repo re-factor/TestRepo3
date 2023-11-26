a434e63f41b1a5f9c79b15bbd5e3ae57ad8d9d37
re-factor <refactor1010@gmail.com>
2023-11-26 14:44:30 -0400

    Update test.py

--- test.py
+++ test.py
@@ -12,3 +12,11 @@ def add_numbers(x, y):
 if __name__ == "__main__":
     """ main method 1234567 """
     print(add_numbers(1, 5))
+
+
+def add_numbers(x, y):
+    return x + y
+
+if __name__ == "__main__":
+    """ main method 1234567 """
+    print(add_numbers(1, 5))
